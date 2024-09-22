498892ce65c731439ffc3677ed923b1c9b8ba706
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2024-09-22 17:18:42 -0300

    chore: configure test environment for Prisma

--- prisma/vitest-environment-prisma/index.ts
+++ prisma/vitest-environment-prisma/index.ts
@@ -1,13 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest/environments'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default {
   name: 'prisma',
   transformMode: 'ssr',
-  setup: () => {
-    console.log('setup')
+  setup: async () => {
+    const schema = randomUUID()
+
+    const databaseUrl = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown: () => {
-        console.log('teardown')
+      teardown: async () => {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
