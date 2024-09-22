526e1fd3939e513aca3b41d6d45d45bbf290ee65
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2024-09-22 17:56:45 -0300

    feat: add middleware to verify JWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   // Authenticated routes
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
